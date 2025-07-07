a5afa5ecd93f9c27b25009732a04f5a3898cee92
xin.huang <activehx@163.com>
2025-07-07 11:06:01 +0800

    Update init.js
    邮箱名称

--- mail-worker/src/init/init.js
+++ mail-worker/src/init/init.js
@@ -327,7 +327,7 @@ const init = {
       INSERT INTO setting (
         register, receive, add_email, many_email, title, auto_refresh_time, register_verify, add_email_verify
       )
-      SELECT 0, 0, 0, 1, 'Cloud 邮箱', 0, 1, 1
+      SELECT 0, 0, 0, 1, 'HarryReid 邮箱', 0, 1, 1
       WHERE NOT EXISTS (SELECT 1 FROM setting)
     `).run();
 	},
